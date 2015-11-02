0c01ce22b64fec36cae5f2594e3d30acc23aace7
m.kawonczyk <m.kawonczyk@samsung.com>
2015-11-02 17:54:55 +0100

    Links opened in new window works wrong
    [Issue]        https://bugs.tizen.org/jira/browse/TT-243
    [Problem]      SwitchToWebPage function called before web page is
                   created.
    [Solution]     Added timer after __newWindowRequest engine function that
                   after 0.01s signals windowCreated. After this signal
                   SwitchToWebPage function is correctly called.
                   There should be engine callback f.e. "window,created"
                   that would tell browser when the window is created.
    [Verification] Click on a link that opens new window or right click
                   on a link and choose open in a new tab.
    
    Change-Id: I4721a4d8842c486c6067d11647db1d486a04af83

--- services/WebKitEngineService/WebKitEngineService.h
+++ services/WebKitEngineService/WebKitEngineService.h
@@ -21,6 +21,7 @@
 #include <string>
 #include <Evas.h>
 #include <memory>
+#include <Ecore.h>
 
 #include "ServiceFactory.h"
 #include "service_macros.h"
@@ -205,6 +206,7 @@ private:
     void _confirmationRequest(WebConfirmationPtr) ;
     void _IMEStateChanged(bool);
     void webViewClicked();
+    static Eina_Bool _windowCreated(void *data);
 
     /**
      * disconnect signals from specified WebView
@@ -236,6 +238,8 @@ private:
     std::list<TabId> m_mostRecentTab;
     // recently added tabs first
     std::list<TabId> m_chronoTabs;
+    // timer for reloading webview correctly
+    Ecore_Timer *m_timer;
 };
 
 } /* end of webkitengine_service */
