1f03aa44ca63ec6eadcb7b5abbf3447d1e2a52da
Maciej Skrzypkowski <m.skrzypkows@samsung.com>
2015-11-06 10:03:07 +0100

    Scroller for QuickAccess mobile view
    [Issue#]   https://bugs.tizen.org/jira/browse/TM-34
    [Problem]  N/A
    [Cause]    N/A
    [Solution] Added scroller and compilation flag for mobile view.
    [Verify]   Open QuickAccess view on mobile, open few pages, check
               if scroller works on QA.
    
    Change-Id: I1f08a38efc057a02f8a7afa1f8effb9b38307dec
    Signed-off-by: Maciej Skrzypkowski <m.skrzypkows@samsung.com>

--- services/QuickAccess/QuickAccess.cpp
+++ services/QuickAccess/QuickAccess.cpp
@@ -73,6 +73,10 @@ QuickAccess::QuickAccess()
     , m_parentFocusChain(nullptr)
     , m_bookmark_item_class(nullptr)
     , m_detailPopup(this)
+#if PROFILE_MOBILE
+    , m_scroller(nullptr)
+    , m_centerLayout(nullptr)
+#endif
 {
     BROWSER_LOGD("%s:%d %s", __FILE__, __LINE__, __func__);
     edjFilePath = EDJE_DIR;
@@ -146,7 +150,7 @@ Evas_Object* QuickAccess::createQuickAccessLayout(Evas_Object* parent)
     return layout;
 }
 
-Evas_Object* QuickAccess::createMostVisitedView (Evas_Object * parent)
+Evas_Object* QuickAccess::createMostVisitedView(Evas_Object * parent)
 {
     BROWSER_LOGD("%s:%d %s", __FILE__, __LINE__, __func__);
 
@@ -157,6 +161,23 @@ Evas_Object* QuickAccess::createMostVisitedView (Evas_Object * parent)
 
     Evas_Object* topButtons = createTopButtons(mostVisitedLayout);
     elm_object_part_content_set(mostVisitedLayout, "elm.swallow.layoutTop", topButtons);
+
+#if PROFILE_MOBILE
+    m_scroller = elm_scroller_add(mostVisitedLayout);
+    evas_object_size_hint_weight_set(m_scroller, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set (m_scroller, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_scroller_policy_set(m_scroller, ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_OFF);
+    elm_scroller_bounce_set(m_scroller, EINA_FALSE, EINA_FALSE);
+    elm_object_part_content_set(mostVisitedLayout, "center_swallow", m_scroller);
+    evas_object_show(m_scroller);
+
+    m_centerLayout = elm_layout_add(m_scroller);
+    elm_layout_file_set(m_centerLayout, edjFilePath.c_str(), "center_layout");
+    evas_object_size_hint_weight_set(m_centerLayout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set (m_centerLayout, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_object_content_set(m_scroller, m_centerLayout);
+    evas_object_show(m_centerLayout);
+#endif
 
     return mostVisitedLayout;
 }
@@ -280,7 +301,11 @@ void QuickAccess::addHistoryItem(std::shared_ptr<services::HistoryItem> hi)
     itemData->item = hi;
     itemData->quickAccess = std::shared_ptr<QuickAccess>(this);
 
+#if PROFILE_MOBILE
+    Evas_Object* tile = elm_button_add(m_centerLayout);
+#else
     Evas_Object* tile = elm_button_add(m_mostVisitedView);
+#endif
     if (tileNumber == BIG_TILE_INDEX)
         elm_object_style_set(tile, "big_tile");
     else
@@ -288,14 +313,18 @@ void QuickAccess::addHistoryItem(std::shared_ptr<services::HistoryItem> hi)
     evas_object_size_hint_weight_set(tile, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
     evas_object_size_hint_align_set (tile, EVAS_HINT_FILL, EVAS_HINT_FILL);
     evas_object_show(tile);
+#if PROFILE_MOBILE
+    elm_object_part_content_set(m_centerLayout, TILES_NAMES[tileNumber].c_str(), tile);
+#else
     elm_object_part_content_set(m_mostVisitedView, TILES_NAMES[tileNumber].c_str(), tile);
+#endif
     m_tiles.push_back(tile);
 
     elm_layout_text_set(tile, "page_title", hi->getTitle().c_str());
     elm_layout_text_set(tile, "page_url", hi->getUrl().c_str());
     Evas_Object * thumb = tizen_browser::tools::EflTools::getEvasImage(hi->getThumbnail(), m_parent);
     elm_object_part_content_set(tile, "elm.thumbnail", thumb);
-    evas_object_smart_callback_add(tile, "clicked", _thumbHistoryClicked, itemData);
+    evas_object_smart_callback_add(tile, "clicked", _thumbMostVisitedClicked, itemData);
 
     m_historyItems.push_back(hi);
 }
@@ -313,6 +342,14 @@ void QuickAccess::addHistoryItems(std::shared_ptr<services::HistoryItemVector> i
     }
     if (i>0)
         setEmptyView(false);
+
+#if PROFILE_MOBILE
+    if (items->size() > FIXED_SIZE_TILES_NUMBER) {
+        elm_object_signal_emit(m_centerLayout, "set_fixed_size", "ui");
+    } else {
+        elm_object_signal_emit(m_centerLayout, "set_dynamic_size", "ui");
+    }
+#endif
 }
 
 void QuickAccess::addBookmarkItem(std::shared_ptr<tizen_browser::services::BookmarkItem> bi)
@@ -371,12 +408,16 @@ void QuickAccess::_thumbBookmarkClicked(void * data, Evas_Object * , void *)
     itemData->quickAccess->m_after_history_thumb = false;
 }
 
-void QuickAccess::_thumbHistoryClicked(void* data, Evas_Object*, void*)
+void QuickAccess::_thumbMostVisitedClicked(void* data, Evas_Object*, void*)
 {
     BROWSER_LOGD("%s:%d %s", __FILE__, __LINE__, __func__);
     HistoryItemData * itemData = reinterpret_cast<HistoryItemData *>(data);
+#if PROFILE_MOBILE
+    itemData->quickAccess->openURLInNewTab(itemData->item, false);
+#else
     itemData->quickAccess->mostVisitedTileClicked(itemData->item, DetailPopup::HISTORY_ITEMS_NO);
     itemData->quickAccess->m_after_history_thumb = true;
+#endif
 }
 
 void QuickAccess::clearHistoryGenlist()
@@ -384,7 +425,7 @@ void QuickAccess::clearHistoryGenlist()
     BROWSER_LOGD("%s:%d %s", __FILE__, __LINE__, __func__);
 
     for (auto it = m_tiles.begin(); it != m_tiles.end(); ++it) {
-        evas_object_smart_callback_del(*it, "clicked", _thumbHistoryClicked);
+        evas_object_smart_callback_del(*it, "clicked", _thumbMostVisitedClicked);
         evas_object_del(*it);
     }
 
