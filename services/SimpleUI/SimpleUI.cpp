bad9cbd2253ccfe147bd2335a9e770d8f27c009f
Gajendra N <gajendra.n@samsung.com>
2015-11-19 15:50:55 +0530

    Implementation of Find on page UI.
    Issue: http://suprem.sec.samsung.net/jira/browse/CBEFL-654
    
    Change-Id: I203165fd49b95d1b7eac1482634bf50b43c40276
    Signed-off-by: Gajendra N <gajendra.n@samsung.com>

--- services/SimpleUI/SimpleUI.cpp
+++ services/SimpleUI/SimpleUI.cpp
@@ -71,6 +71,9 @@ SimpleUI::SimpleUI()
     , m_historyUI()
     , m_settingsUI()
     , m_tabUI()
+#if PROFILE_MOBILE
+    , m_findOnPageUI()
+#endif
     , m_initialised(false)
     , m_wvIMEStatus(false)
     , m_ewkContext(ewk_context_new())
@@ -224,6 +227,13 @@ void SimpleUI::loadUIServices()
         <tizen_browser::base_ui::SettingsUI,tizen_browser::core::AbstractService>
         (tizen_browser::core::ServiceManager::getInstance().getService("org.tizen.browser.settingsui"));
 
+#if PROFILE_MOBILE
+    m_findOnPageUI =
+        std::dynamic_pointer_cast
+        <tizen_browser::base_ui::FindOnPageUI,tizen_browser::core::AbstractService>
+        (tizen_browser::core::ServiceManager::getInstance().getService("org.tizen.browser.findonpageui"));
+#endif
+
     m_moreMenuUI =
         std::dynamic_pointer_cast
         <tizen_browser::base_ui::MoreMenuUI,tizen_browser::core::AbstractService>
@@ -305,6 +315,9 @@ void SimpleUI::connectUISignals()
     m_moreMenuUI->bookmarkManagerClicked.connect(boost::bind(&SimpleUI::showBookmarkManagerUI, this));
     m_moreMenuUI->historyUIClicked.connect(boost::bind(&SimpleUI::showHistoryUI, this));
     m_moreMenuUI->settingsClicked.connect(boost::bind(&SimpleUI::showSettingsUI, this));
+#if PROFILE_MOBILE
+    m_moreMenuUI->findOnPageClicked.connect(boost::bind(&SimpleUI::showFindOnPageUI, this));
+#endif
     m_moreMenuUI->closeMoreMenuClicked.connect(boost::bind(&SimpleUI::closeMoreMenu, this));
     m_moreMenuUI->switchToMobileMode.connect(boost::bind(&SimpleUI::switchToMobileMode, this));
     m_moreMenuUI->switchToDesktopMode.connect(boost::bind(&SimpleUI::switchToDesktopMode, this));
@@ -312,6 +325,12 @@ void SimpleUI::connectUISignals()
     m_moreMenuUI->isBookmark.connect(boost::bind(&SimpleUI::checkBookmark, this));
     m_moreMenuUI->deleteBookmark.connect(boost::bind(&SimpleUI::deleteBookmark, this));
     m_moreMenuUI->zoomUIClicked.connect(boost::bind(&SimpleUI::showZoomUI, this));
+
+#if PROFILE_MOBILE
+    M_ASSERT(m_findOnPageUI.get());
+    m_findOnPageUI->closeFindOnPageUIClicked.connect(boost::bind(&SimpleUI::closeFindOnPageUI, this));
+    m_findOnPageUI->startFindingWord.connect(boost::bind(&SimpleUI::findWord, this, _1));
+#endif
 
     M_ASSERT(m_bookmarkManagerUI.get());
     m_bookmarkManagerUI->closeBookmarkManagerClicked.connect(boost::bind(&SimpleUI::closeBookmarkManagerUI, this));
@@ -385,6 +404,11 @@ void SimpleUI::initUIServices()
 
     M_ASSERT(m_settingsUI.get());
     m_settingsUI->init(m_viewManager->getContent());
+
+#if PROFILE_MOBILE
+    M_ASSERT(m_findOnPageUI.get());
+    m_findOnPageUI->init(m_viewManager->getContent());
+#endif
 
     M_ASSERT(m_bookmarkManagerUI.get());
     m_bookmarkManagerUI->init(m_viewManager->getContent());
@@ -842,6 +866,28 @@ void SimpleUI::scrollView(const int& dx, const int& dy)
     m_webEngine->scrollView(dx, dy);
 }
 
+#if PROFILE_MOBILE
+void SimpleUI::showFindOnPageUI()
+{
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
+    M_ASSERT(m_findOnPageUI);
+    m_findOnPageUI->show();
+}
+
+void SimpleUI::findWord(const struct FindData& fdata)
+{
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
+    m_webEngine->findWord(fdata.input_str, fdata.forward, fdata.func, fdata.data);
+}
+
+void SimpleUI::closeFindOnPageUI()
+{
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
+    M_ASSERT(m_findOnPageUI);
+    m_findOnPageUI->hideUI();
+}
+#endif
+
 void SimpleUI::showTabUI()
 {
     BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
@@ -1022,6 +1068,7 @@ void SimpleUI::showMoreMenu()
     if (evas_object_visible_get(m_moreMenuUI->getContent()))
         m_moreMenuUI->hideUI();
     else {
+        m_moreMenuUI->shouldShowFindOnPage(!m_webEngine->getURI().empty() ? true : false);
         m_moreMenuUI->blockThumbnails(m_webPageUI->stateEquals(WPUState::QUICK_ACCESS));
         m_moreMenuUI->showUI();
     }
