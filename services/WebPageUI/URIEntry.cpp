a0d18f2a95016277391fa9b71d7329b2199ccde4
Albert Malewski <a.malewski@samsung.com>
2015-11-23 08:47:42 +0100

    Fixed URI Entry's implementation
    [Issue]    https://bugs.tizen.org/jira/browse/TM-106
    [Problem]  URI Entry is not editable.
    [Solution] Removed ellypsis attribute from font style. Moved 'Cancel'
               and 'Incognito' icons to uri_entry_layout.
               Used workaround: standard entry has a border and it is impossible
               to hide it, so I covered it by rectangles.
    [Verify]   Type text in URI Entry. Entry should be editable. You should
               be able to scroll it. The text should not be covered by
               'Cancel'/'Incognito' icons.
    
    Change-Id: I9858db1919604a30264d8ccc2e774e37664c013f

--- services/WebPageUI/URIEntry.cpp
+++ services/WebPageUI/URIEntry.cpp
@@ -87,10 +87,12 @@ Evas_Object* URIEntry::getContent()
         evas_object_smart_callback_add(m_entry, "focused", URIEntry::focused, this);
         evas_object_smart_callback_add(m_entry, "unfocused", URIEntry::unfocused, this);
         evas_object_smart_callback_add(m_entry, "clicked", _uri_entry_clicked, this);
+        evas_object_smart_callback_add(m_entry, "clicked,double", _uri_entry_double_clicked, this);
         evas_object_event_callback_priority_add(m_entry, EVAS_CALLBACK_KEY_DOWN, 2 * EVAS_CALLBACK_PRIORITY_BEFORE, URIEntry::_fixed_entry_key_down_handler, this);
 
         elm_object_part_content_set(m_entry_layout, "uri_entry_swallow", m_entry);
 
+#if !PROFILE_MOBILE
         m_entryBtn = elm_button_add(m_entry_layout);
 
         evas_object_smart_callback_add(m_entryBtn, "focused", URIEntry::focusedBtn, this);
@@ -100,6 +102,7 @@ Evas_Object* URIEntry::getContent()
         evas_object_smart_callback_add(m_entryBtn, "clicked", _uri_entry_btn_clicked, this);
 
         elm_object_part_content_set(m_entry_layout, "uri_entry_btn", m_entryBtn);
+#endif
     }
     return m_entry_layout;
 }
@@ -189,6 +192,7 @@ URIEntry::IconType URIEntry::getCurrentIconTyep()
 
 void URIEntry::selectWholeText()
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     m_oryginalEntryText = elm_entry_markup_to_utf8(elm_entry_entry_get(m_entry));
     if (!m_entrySelectedAllFirst && !m_oryginalEntryText.empty()) {
         elm_entry_select_all(m_entry);
@@ -199,18 +203,31 @@ void URIEntry::selectWholeText()
 
 void URIEntry::_uri_entry_clicked(void* data, Evas_Object* /* obj */, void* /* event_info */)
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     URIEntry* self = static_cast<URIEntry*>(data);
+#if PROFILE_MOBILE
+    self->showCancelIcon();
+    if (self->m_entrySelectedAllFirst) {
+        self->m_entrySelectedAllFirst = false;
+        return;
+    }
+    elm_entry_select_none(self->m_entry);
+#else
     self->selectWholeText();
+#endif
 }
 
+#if !PROFILE_MOBILE
 void URIEntry::_uri_entry_btn_clicked(void* data, Evas_Object* /*obj*/, void* /*event_info*/)
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     URIEntry* self = static_cast<URIEntry*>(data);
     elm_object_focus_set(self->m_entry, EINA_TRUE);
     self->selectWholeText();
 
     elm_object_signal_emit(self->m_entry_layout, "mouse,in", "over");
 }
+#endif
 
 void URIEntry::activated(void* /* data */, Evas_Object* /* obj */, void* /*event_info*/)
 {
@@ -241,11 +258,13 @@ void URIEntry::_uri_entry_editing_changed_user(void* data, Evas_Object* /* obj *
     } else {//if(entry.find(" ") != std::string::npos){
         self->setSearchIcon();
     }
+    self->showCancelIcon();
     self->uriEntryEditingChangedByUser(std::make_shared<std::string>(entry));
 }
 
 void URIEntry::setUrlGuideText(const char* txt) const
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
 #if PLATFORM(TIZEN)
     elm_object_translatable_part_text_set(m_entry, "elm.guide", txt);
 #else
@@ -255,6 +274,7 @@ void URIEntry::setUrlGuideText(const char* txt) const
 
 void URIEntry::unfocused(void* data, Evas_Object*, void*)
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     URIEntry* self = static_cast<URIEntry*>(data);
     self->m_entrySelectedAllFirst = false;
     self->setUrlGuideText(GUIDE_TEXT_UNFOCUSED);
@@ -267,6 +287,7 @@ void URIEntry::unfocused(void* data, Evas_Object*, void*)
 
 void URIEntry::focused(void* data, Evas_Object* /* obj */, void* /* event_info */)
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     URIEntry* self = static_cast<URIEntry*>(data);
     self->setUrlGuideText(GUIDE_TEXT_FOCUSED);
     elm_object_signal_emit(self->m_entry_layout, "mouse,in", "over");
@@ -297,20 +318,25 @@ void URIEntry::_fixed_entry_key_down_handler(void* data, Evas* /*e*/, Evas_Objec
     }
     if (keynameEsc == ev->keyname) {
         self->editingCanceled();
+#if !PROFILE_MOBILE
         elm_object_focus_set(self->m_entryBtn, EINA_TRUE);
+#endif
         return;
     }
 }
 
 void URIEntry::editingCompleted()
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     char* text = elm_entry_markup_to_utf8(elm_entry_entry_get(m_entry));
     std::string userString(text);
     free(text);
 
     elm_entry_input_panel_hide(m_entry);
     uriChanged(rewriteURI(userString));
+#if !PROFILE_MOBILE
     elm_object_focus_set(m_entryBtn, EINA_TRUE);
+#endif
 }
 
 std::string URIEntry::rewriteURI(const std::string& url)
@@ -359,34 +385,43 @@ std::list<sharedAction> URIEntry::actions() const
 
 void URIEntry::clearFocus()
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     elm_object_focus_set(m_entry, EINA_FALSE);
 }
 
 void URIEntry::setFocus()
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
+#if !PROFILE_MOBILE
     elm_object_focus_set(m_entryBtn, EINA_TRUE);
+#endif
 }
 
 bool URIEntry::hasFocus() const
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     return elm_object_focus_get(m_entry) == EINA_TRUE ? true : false;
 }
 
+#if !PROFILE_MOBILE
 void URIEntry::focusedBtn(void* data, Evas_Object* /*obj*/, void* /*event_info*/)
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     URIEntry* self = static_cast<URIEntry*>(data);
     elm_object_signal_emit(self->m_entry_layout, "mouse,in", "over");
 }
 
 void URIEntry::unfocusedBtn(void* data, Evas_Object* /*obj*/, void* /*event_info*/)
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     URIEntry* self = static_cast<URIEntry*>(data);
     elm_object_signal_emit(self->m_entry_layout, "mouse,out", "over");
-    //elm_entry_entry_set(self->m_entry, elm_entry_utf8_to_markup(self->m_pageTitle.c_str()));
 }
+#endif
 
 void URIEntry::setDisabled(bool disabled)
 {
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     if (disabled) {
         clearFocus();
     }
@@ -399,6 +434,24 @@ void URIEntry::_uri_cancel_icon_clicked(void* data, Evas_Object* /*obj*/, const 
     BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
     URIEntry* self = static_cast<URIEntry*>(data);
     elm_entry_entry_set(self->m_entry, "");
+    elm_object_signal_emit(self->m_entry_layout, "hide_cancel_icon", "ui");
+}
+
+void URIEntry::_uri_entry_double_clicked(void* data, Evas_Object* /*obj*/, void* /*event_info*/)
+{
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
+    URIEntry* self = static_cast<URIEntry*>(data);
+    self->selectWholeText();
+}
+
+void URIEntry::showCancelIcon()
+{
+    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
+    bool isEntryEmpty = elm_entry_is_empty(m_entry);
+    if(!isEntryEmpty)
+        elm_object_signal_emit(m_entry_layout, "show_cancel_icon", "ui");
+    else
+        elm_object_signal_emit(m_entry_layout, "hide_cancel_icon", "ui");
 }
 #endif
 
