228ba1449ea0f4b7fcd40b1585bc86fa35dc2e28
Albert Malewski <a.malewski@samsung.com>
2015-12-10 10:18:44 +0100

    Resizing a webview when IME is opened/closed
    [Issue]    http://web.sec.samsung.net/bugzilla/show_bug.cgi?id=15139
    [Problem]  Botton input field is hidden by IME.
    [Cause]    Platform's problem with conformants.
    [Solution] Resizing webview on "editorclient,ime,opened" and
               "editorclient,ime,closed".
    [Verify]   Load a page > Click an input field on the page > Obs
               A page should be resized.
    
    Change-Id: Ib4ec1f2a4cda2c3318c5e2fe340d0209896ef3ff

--- services/SimpleUI/SimpleUI.cpp
+++ services/SimpleUI/SimpleUI.cpp
@@ -785,6 +785,10 @@ void SimpleUI::setwvIMEStatus(bool status)
 {
     BROWSER_LOGD("[%s]", __func__);
     m_wvIMEStatus = status;
+    if (status)
+        m_webPageUI->decreaseWebview();
+    else
+        m_webPageUI->enlargeWebview();
 }
 
 void SimpleUI::onBackPressed()
