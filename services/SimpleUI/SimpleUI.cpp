c788c570e1f62217b14eb0a590150b6591a51d03
Kamil Nowac <k.nowac@samsung.com>
2015-12-17 13:43:16 +0100

    Webview does not resize after enering the tab manager
    [Issue]    https://bugs.tizen.org/jira/browse/TM-171
    [Problem]  WebView does not resize
    [Solution] Clear text selection
    [Verify]   Open google.com -> click on search bar ->
               Keyboard should be visible -> click on
               tab manager icon -> close tab manager ->
               Result: WebView should be properly resized
               to fit the screen
    
    Change-Id: Id1e1c7ed62c809d0f631e36afeafc6ba6d1dde7d

--- services/SimpleUI/SimpleUI.cpp
+++ services/SimpleUI/SimpleUI.cpp
@@ -1101,6 +1101,7 @@ void SimpleUI::closeFindOnPageUI()
 void SimpleUI::showTabUI()
 {
     BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
+    m_webEngine->clearCurrentWebViewTextSelection();
     m_viewManager.pushViewToStack(m_tabUI.get());
 
     std::vector<basic_webengine::TabContentPtr> tabsContents =
