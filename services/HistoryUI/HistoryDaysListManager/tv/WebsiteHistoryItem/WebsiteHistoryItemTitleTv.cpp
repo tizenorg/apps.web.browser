93abe1c31896f4e309f1d729999b2cc3f5b7e2cb
Adam Skobodzinski <a.skobodzins@partner.samsung.com>
2015-12-21 12:36:04 +0100

    The HistoryManager: drawing 'clear' image on focused items.
    [Issue]    https://bugs.tizen.org/jira/browse/TT-259
    [Problem]  When in 'clear mode', 'clear' images should be displayed on
               the focused items.
    [Solution] Added images to edc groups describing items, which can be
               deleted. Displaying them when 'clear history' toggled 'clearging
               mode'.
    [Verify]   Choose 'clear history' button. FOcus on history items (image
               should be displayed next to day label, website title box
               and visit item box). Toggle button off: images should not
               be displayed.
    
    This commit only displays images. No clearing history items yet.
    Also added proper clearing of Evas_Object* objects.
    
    Change-Id: I3f663e90e1b9937a296363943d4dfc152ab872da
    Signed-off-by: Adam Skobodzinski <a.skobodzins@partner.samsung.com>

--- services/HistoryUI/HistoryDaysListManager/tv/WebsiteHistoryItem/WebsiteHistoryItemTitleTv.cpp
+++ services/HistoryUI/HistoryDaysListManager/tv/WebsiteHistoryItem/WebsiteHistoryItemTitleTv.cpp
@@ -17,6 +17,7 @@
 #include "WebsiteHistoryItemTitleTv.h"
 #include "../../HistoryDayItemData.h"
 #include <EflTools.h>
+#include <services/HistoryUI/HistoryDeleteManager.h>
 
 namespace tizen_browser{
 namespace base_ui{
@@ -25,8 +26,14 @@ boost::signals2::signal<void(const WebsiteHistoryItemDataPtr)>
 WebsiteHistoryItemTitleTv::signalWebsiteHistoryItemClicked;
 
 WebsiteHistoryItemTitleTv::WebsiteHistoryItemTitleTv(
-        WebsiteHistoryItemDataPtr websiteHistoryItemData)
+        WebsiteHistoryItemDataPtr websiteHistoryItemData,
+        HistoryDeleteManagerPtrConst historyDeleteManager)
     : m_websiteHistoryItemData(websiteHistoryItemData)
+    , m_buttonSelect(nullptr)
+    , m_imageClear(nullptr)
+    , m_layoutMain(nullptr)
+    , m_boxMainHorizontal(nullptr)
+    , m_historyDeleteManager(historyDeleteManager)
 {
 }
 
@@ -38,37 +45,79 @@ WebsiteHistoryItemTitleTv::~WebsiteHistoryItemTitleTv()
 Evas_Object* WebsiteHistoryItemTitleTv::init(Evas_Object* parent,
         const std::string& edjeFilePath)
 {
-    m_layoutHistoryItemTitle = elm_layout_add(parent);
-    tools::EflTools::setExpandHints(m_layoutHistoryItemTitle);
-    elm_layout_file_set(m_layoutHistoryItemTitle, edjeFilePath.c_str(),
+    m_layoutMain = elm_layout_add(parent);
+    tools::EflTools::setExpandHints(m_layoutMain);
+    elm_layout_file_set(m_layoutMain, edjeFilePath.c_str(),
             "layoutWebsiteHistoryItemTitle");
-    evas_object_size_hint_align_set(m_layoutHistoryItemTitle, 0.0, 0.0);
+    evas_object_size_hint_align_set(m_layoutMain, 0.0, 0.0);
 
     m_boxMainHorizontal = elm_box_add(parent);
     tools::EflTools::setExpandHints(m_boxMainHorizontal);
     elm_box_align_set(m_boxMainHorizontal, 0.0, 0.0);
     elm_box_horizontal_set(m_boxMainHorizontal, EINA_TRUE);
-    elm_object_part_content_set(m_layoutHistoryItemTitle, "boxMainHorizontal",
+    elm_object_part_content_set(m_layoutMain, "boxMainHorizontal",
             m_boxMainHorizontal);
 
     m_buttonSelect = elm_button_add(parent);
-    elm_object_part_content_set(m_layoutHistoryItemTitle, "buttonSelect",
+    elm_object_part_content_set(m_layoutMain, "buttonSelect",
             m_buttonSelect);
     evas_object_color_set(m_buttonSelect, 0, 0, 0, 0);
     elm_object_style_set(m_buttonSelect, "anchor");
+
+    m_imageClear = createImageClear(parent, edjeFilePath);
+    elm_object_part_content_set(m_layoutMain, "imageClear",
+            m_imageClear);
 
     Evas_Object* layoutIcon = createLayoutIcon(parent, edjeFilePath);
     Evas_Object* layoutSummary = createLayoutSummary(parent, edjeFilePath);
     elm_box_pack_end(m_boxMainHorizontal, layoutIcon);
     elm_box_pack_end(m_boxMainHorizontal, layoutSummary);
 
+    evas_object_show(m_buttonSelect);
     evas_object_show(layoutIcon);
     evas_object_show(layoutSummary);
-    evas_object_show(m_layoutHistoryItemTitle);
+    evas_object_show(m_layoutMain);
 
     initCallbacks();
+    return m_layoutMain;
+}
 
-    return m_layoutHistoryItemTitle;
+void WebsiteHistoryItemTitleTv::initCallbacks()
+{
+    evas_object_smart_callback_add(m_buttonSelect, "clicked",
+            _buttonSelectClicked, &m_websiteHistoryItemData);
+    evas_object_smart_callback_add(m_buttonSelect, "focused",
+            _buttonSelectFocused, this);
+    evas_object_smart_callback_add(m_buttonSelect, "unfocused",
+            _buttonSelectUnfocused, this);
+}
+
+void WebsiteHistoryItemTitleTv::_buttonSelectClicked(void* data,
+        Evas_Object* /*obj*/, void* /*event_info*/)
+{
+    WebsiteHistoryItemDataPtr* websiteHistoryItemData =
+            static_cast<WebsiteHistoryItemDataPtr*> (data);
+    signalWebsiteHistoryItemClicked(*websiteHistoryItemData);
+}
+
+void WebsiteHistoryItemTitleTv::_buttonSelectFocused(void* data,
+        Evas_Object* /*obj*/, void* /*event_info*/)
+{
+    WebsiteHistoryItemTitleTv* self = static_cast<WebsiteHistoryItemTitleTv*>(data);
+    Evas_Object* layoutMain = self->getLayoutMain();
+    elm_object_signal_emit(layoutMain, "buttonSelectFocused", "ui");
+    if(self->getDeleteManager()->getDeleteMode()) {
+        elm_object_signal_emit(layoutMain, "showImageClear", "ui");
+    }
+}
+
+void WebsiteHistoryItemTitleTv::_buttonSelectUnfocused(void* data,
+        Evas_Object* /*obj*/, void* /*event_info*/)
+{
+    WebsiteHistoryItemTitleTv* self =
+            static_cast<WebsiteHistoryItemTitleTv*>(data);
+    Evas_Object* layoutMain = self->getLayoutMain();
+    elm_object_signal_emit(layoutMain, "buttonSelectUnfocused", "ui");
 }
 
 Evas_Object* WebsiteHistoryItemTitleTv::createLayoutIcon(Evas_Object* parent,
@@ -114,22 +163,17 @@ Evas_Object* WebsiteHistoryItemTitleTv::createLayoutSummary(Evas_Object* parent,
     return layout;
 }
 
-void WebsiteHistoryItemTitleTv::initCallbacks()
+Evas_Object* WebsiteHistoryItemTitleTv::createImageClear(Evas_Object* parent,
+        const std::string& edjeFilePath)
 {
-    evas_object_smart_callback_add(m_buttonSelect, "clicked",
-            _buttonSelectClicked, &m_websiteHistoryItemData);
+    Evas_Object* imageClear = elm_image_add(parent);
+    elm_image_file_set(imageClear, edjeFilePath.c_str(), "groupImageClear");
+    return imageClear;
 }
 
 void WebsiteHistoryItemTitleTv::deleteCallbacks()
 {
     evas_object_smart_callback_del(m_buttonSelect, "clicked", NULL);
-}
-
-void WebsiteHistoryItemTitleTv::_buttonSelectClicked(void* data,
-        Evas_Object* /*obj*/, void* /*event_info*/)
-{
-    WebsiteHistoryItemDataPtr* websiteHistoryItemData = static_cast<WebsiteHistoryItemDataPtr*>(data);
-    signalWebsiteHistoryItemClicked(*websiteHistoryItemData);
 }
 
 void WebsiteHistoryItemTitleTv::setFocusChain(Evas_Object* obj)
