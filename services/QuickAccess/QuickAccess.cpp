f9089dafe9166209f63ea4f2851242bc49248fc6
Albert Malewski <a.malewski@samsung.com>
2016-01-14 13:52:29 +0100

    xMade browser using base_scale instead of config_scale
    [Issue]    http://107.108.218.239/bugzilla/show_bug.cgi?id=15396
    [Problem]  Strings on datalist popup are small
    [Cause]    Config scale is used instead of base_scale
    [Solution] Refactored some widgets to be compatible with base_scale
    [Verify]   Launch browser and check if every widgets have correct size.
               Attention! QuickAccess, UrlHistoryList and Auto fill form are
               not refactored yet. They need to be fixed!
    
    Change-Id: Icc4b7b50ebe8e6e14ff87c8d3d4604e05102f08e

--- services/QuickAccess/QuickAccess.cpp
+++ services/QuickAccess/QuickAccess.cpp
@@ -27,8 +27,6 @@
 #include "../Tools/BrowserImage.h"
 #include "Tools/GeneralTools.h"
 
-#define efl_scale       (elm_config_scale_get() / elm_app_base_scale_get())
-
 namespace tizen_browser{
 namespace base_ui{
 
@@ -243,6 +241,7 @@ Evas_Object* QuickAccess::createBookmarksView (Evas_Object * parent)
 
     m_bookmarkGengrid = createBookmarkGengrid(bookmarkViewLayout);
     elm_object_part_content_set(bookmarkViewLayout, "elm.swallow.grid", m_bookmarkGengrid);
+    evas_object_size_hint_min_set(m_bookmarkGengrid, ELM_SCALE_SIZE(720), ELM_SCALE_SIZE(1150));
     evas_object_show(m_bookmarkGengrid);
 
     return bookmarkViewLayout;
@@ -260,13 +259,13 @@ Evas_Object* QuickAccess::createBookmarkGengrid(Evas_Object *parent)
     evas_object_size_hint_align_set(bookmarkGengrid, EVAS_HINT_FILL, EVAS_HINT_FILL);
 
 #if !PROFILE_MOBILE
-    elm_scroller_page_size_set(bookmarkGengrid, 0, 327);
-    elm_gengrid_item_size_set(bookmarkGengrid, 364 * efl_scale, 320 * efl_scale);
+    elm_scroller_page_size_set(bookmarkGengrid, ELM_SCALE_SIZE((0), ELM_SCALE_SIZE(327));
+    elm_gengrid_item_size_set(bookmarkGengrid, ELM_SCALE_SIZE(364), ELM_SCALE_SIZE(320));
     elm_gengrid_align_set(bookmarkGengrid, 0.5, 0.5);
 #else
-    elm_scroller_page_size_set(bookmarkGengrid, 0, 1100);
+    elm_scroller_page_size_set(bookmarkGengrid, ELM_SCALE_SIZE(0), ELM_SCALE_SIZE(1100));
     elm_gengrid_align_set(bookmarkGengrid, 0.5, 0.0);
-    elm_gengrid_item_size_set(bookmarkGengrid, 337 * efl_scale, 379 * efl_scale);
+    elm_gengrid_item_size_set(bookmarkGengrid, ELM_SCALE_SIZE(337), ELM_SCALE_SIZE(379));
 #endif
     elm_scroller_bounce_set(bookmarkGengrid, EINA_FALSE, EINA_FALSE);
 
