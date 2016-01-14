93b211a79cee74468f42adb0b95c16904583c975
Maciej Skrzypkowski <m.skrzypkows@samsung.com>
2016-01-14 09:48:55 +0100

    Rotatable interface
    [Issue]    https://bugs.tizen.org/jira/browse/TM-141
    [Problem]  There was no common inteface for rotatable classes.
    [Solution] Added AbstractRotatable interface which should implement
               all classes which react on rotation event.
    [Verify]   Build browser.
    
    Change-Id: I5e0e976fc3e9d6aa309a9c6c211886abad62d7f2
    Signed-off-by: Maciej Skrzypkowski <m.skrzypkows@samsung.com>

---
+++ core/AbstractInterfaces/AbstractRotatable.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ABSTRACT_ROTATABLE_H__
+#define __ABSTRACT_ROTATABLE_H__
+
+namespace tizen_browser
+{
+namespace interfaces
+{
+
+/**
+ * @brief This interface defines rotatable object, 
+ * which reacts on mobile device oriantation change.
+ */
+class AbstractRotatable
+{
+public:
+    /**
+     * @brief Sets the parent which will be used when getContent is used.
+     */
+    virtual void rotate(bool portrait) = 0;
+};
+
+}//namespace interfaces
+}//namespace tizen_browser
+
+#endif /* __ABSTRACT_ROTATABLE_H__ */
