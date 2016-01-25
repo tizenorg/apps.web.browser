030d5fad169bb5872a47e8a05c18f9116a565a04
Gajendra N <gajendra.n@samsung.com>
2016-01-25 15:43:17 +0530

    Add 'Developer options' item to Settings menu, to override UserAgent string.
    [Issue]    https://bugs.tizen.org/jira/browse/TM-210
    [Problem]  There was no option in Settings to override UserAgent string.
    [Solution] Added a menu item to perform the above operation.
    [Verify]   Open Settings -> Developer options -> Click the button ->
               Update UA and click done.
    
    Change-Id: I5dd754f68a743328cde0e4b7723a4ef3154e2821
    Signed-off-by: Gajendra N <gajendra.n@samsung.com>

--- services/SimpleUI/SimpleUI.h
+++ services/SimpleUI/SimpleUI.h
@@ -295,9 +295,11 @@ private:
     void settingsDeleteSelectedData(const std::string& str);
     void settingsResetMostVisited();
     void settingsResetBrowser();
+    void settingsOverrideUseragent();
     void onDeleteSelectedDataButton(const std::string &dataText);
     void onDeleteMostVisitedButton(std::shared_ptr<PopupData> popupData);
     void onResetBrowserButton(PopupButtons button, std::shared_ptr<PopupData> popupData);
+    void onOverrideUseragentButton(const std::string& str);
     void tabLimitPopupButtonClicked(PopupButtons button, std::shared_ptr< PopupData > /*popupData*/);
     int tabsCount();
 
