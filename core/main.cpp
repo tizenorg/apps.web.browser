b976c607a2c793cb76d6d78abed5aed60aff163a
Gajendra N <gajendra.n@samsung.com>
2016-02-02 11:04:21 +0530

    Disable COREGL_FASTPATH env until video black screen issue is resolved.
    Change-Id: I9fa16fae161a1eb98705c662fb80cccb7061e01b
    Signed-off-by: Gajendra N <gajendra.n@samsung.com>

--- core/main.cpp
+++ core/main.cpp
@@ -181,7 +181,8 @@ int main(int argc, char* argv[])try
 	BROWSER_LOGD("BROWSER TAG: %s",tizen_browser::logger::Logger::getInstance().getLogTag().c_str());
 	BROWSER_LOGD("BROWSER REGISTERED LOGGERS COUNT: %d",tizen_browser::logger::Logger::getInstance().getLoggersCount());
 
-    setenv("COREGL_FASTPATH", "1", 1);
+    // TODO: Enable it after video black screen issue is resolved.
+    // setenv("COREGL_FASTPATH", "1", 1);
 
     ui_app_lifecycle_callback_s ops;
     memset(&ops, 0x00, sizeof(ui_app_lifecycle_callback_s));
