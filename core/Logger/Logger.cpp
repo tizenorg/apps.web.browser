f36c519bd7489c3a82fc5f559f1697e982545d78
cookie <cookie@samsung.com>
2016-03-09 13:47:58 +0900

    Fix the Svace WID  917124 / 917149 / 917151 / 917416
    http://165.213.149.170/jira/i#browse/TWF-533
    
    Change-Id: I185eb985c29f9d256e9916ab4b0135c0de810477
    Signed-off-by: cookie <cookie@samsung.com>

--- core/Logger/Logger.cpp
+++ core/Logger/Logger.cpp
@@ -149,13 +149,16 @@ int Logger::registerLogger(AbstractLogger *l) {
 
 std::string Logger::timeStamp() {
 	time_t initializer = time(NULL);
-	tm *b = localtime(&initializer);
+	 struct tm b;
+	 if(localtime_r(&initializer,&b)==NULL){
+	     return NULL;
+	 }
     struct timeval detail_time;
     gettimeofday(&detail_time,NULL);
 
 	char buf[80];
 //	strftime(buf, sizeof(buf), "%d/%m/%y,%T ", brokenTime, detail_time.tv_usec/1000);
-        snprintf(buf,  sizeof(buf),"[%d/%d/%d,%d:%d:%d.%ld]", b->tm_year, b->tm_mon, b->tm_mday, b->tm_hour, b->tm_min, b->tm_sec, detail_time.tv_usec/1000);
+        snprintf(buf,  sizeof(buf),"[%d/%d/%d,%d:%d:%d.%ld]", b.tm_year, b.tm_mon, b.tm_mday, b.tm_hour, b.tm_min, b.tm_sec, detail_time.tv_usec/1000);
 	return std::string(buf);
 }
 
