2af7a53ce7f89bf685e11a977b3324a6acc28e3e
m.kawonczyk <m.kawonczyk@samsung.com>
2016-04-13 11:53:28 +0200

    Favicon mask is visible in history manager
    [Issue]    http://suprem.sec.samsung.net/jira/browse/TWF-794
    [Problem]  There was no mask for selected item.
    [Solution] Add new mask + connect it with efl mouse signals.
    [Verify]   Go to history manager and press history. Mask should have
               same background as the rest of the selected area.
    
    Change-Id: I69b1edc696f30cfdd30e524be0dc93a0a416c970

--- services/HistoryUI/HistoryDaysListManager/mob/WebsiteHistoryItem/WebsiteHistoryItemTitleMob.h
+++ services/HistoryUI/HistoryDaysListManager/mob/WebsiteHistoryItem/WebsiteHistoryItemTitleMob.h
@@ -58,7 +58,9 @@ private:
             const std::string& edjeFilePath);
     void initCallbacks();
 
-    static void _buttonSelectClicked(void* data, Evas_Object* obj, 
+    static void _title_mouse_down(void* data, Evas*, Evas_Object* obj, void* event_info);
+    static void _title_mouse_up(void* data, Evas*, Evas_Object* obj, void* event_info);
+    static void _buttonSelectClicked(void* data, Evas_Object* obj,
             void* event_info);
     static void _buttonDeleteClicked(void* data, Evas_Object* obj,
             void* event_info);
