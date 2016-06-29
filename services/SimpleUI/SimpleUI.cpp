ceefdf2183b2ff5fc8ca1874cf1666271e7d1b3e
m.kawonczyk <m.kawonczyk@samsung.com>
2016-06-29 15:28:30 +0200

    Revert "[Workaround]   Highlight text color is gray"
    This reverts commit 4ea914ed7b88c6eed9e15593b33ea7199b342a63.
    
    Change-Id: Ibfdb50909accb3128df63147263dbc312650e4e5

--- services/SimpleUI/SimpleUI.cpp
+++ services/SimpleUI/SimpleUI.cpp
@@ -82,7 +82,6 @@ SimpleUI::SimpleUI()
     , m_tabLimit(0)
     , m_favoritesLimit(0)
     , m_wvIMEStatus(false)
-    , m_webEngineHadFocusBeforeSuspend(false)
 #if PROFILE_MOBILE
     , m_current_angle(0)
     , m_temp_angle(0)
@@ -130,30 +129,16 @@ SimpleUI::~SimpleUI() {
 
 void SimpleUI::suspend()
 {
-    //TODO: Delete when web_view fixed unfocus on suspend issue
-    m_webEngineHadFocusBeforeSuspend = m_webEngine->hasFocus();
     m_webEngine->suspend();
 }
 
 void SimpleUI::resume()
 {
     m_webEngine->resume();
-    //TODO: Delete when web_view fixed unfocus on suspend issue
-    if (m_webEngineHadFocusBeforeSuspend)
-        m_webEngineFocusWorkaroundTimer =  ecore_timer_add(0.0, web_view_set_focus_timer, this);
-
 #if PROFILE_MOBILE
     if (m_findOnPageUI)
         m_findOnPageUI->show_ime();
 #endif
-}
-
-Eina_Bool SimpleUI::web_view_set_focus_timer(void *data)
-{
-    BROWSER_LOGD("[%s:%d] ", __PRETTY_FUNCTION__, __LINE__);
-    SimpleUI * simpleUI = static_cast<SimpleUI*>(data);
-    simpleUI->m_webEngine->setFocus();
-    return ECORE_CALLBACK_CANCEL;
 }
 
 void SimpleUI::destroyUI()
