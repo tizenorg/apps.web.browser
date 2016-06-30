babb2b5761ddc334d899d0ed0dfcc66209846299
Hye Kyoung Hwang <cookie@samsung.com>
2016-06-29 19:35:12 -0700

    Revert "Revert "[Workaround]   Highlight text color is gray""
    This reverts commit 909739dbb63c1c3da9368e00645e8375586378eb.
    
    Change-Id: Idf889da0b7e199fe6554853b44315769c6147c79

--- services/SimpleUI/SimpleUI.h
+++ services/SimpleUI/SimpleUI.h
@@ -87,6 +87,7 @@ public:
     void resume();
 
     void destroyUI();
+    static Eina_Bool web_view_set_focus_timer(void *data);
 private:
     // setup functions
     void loadUIServices();
@@ -360,6 +361,7 @@ private:
     int m_tabLimit;
     int m_favoritesLimit;
     bool m_wvIMEStatus;
+    bool m_webEngineHadFocusBeforeSuspend;
     std::string m_folder_name;
 
     //helper object used to view management
@@ -370,6 +372,7 @@ private:
     int m_current_angle;
     int m_temp_angle;
 #endif
+    Ecore_Timer* m_webEngineFocusWorkaroundTimer;
 };
 
 }
